5f316fde139fe42e6b8ff94de8c82044bcf1b524
usuario <usuario@10.22.143.36>
2015-10-14 10:11:33 +0100

    Third commit

--- src/kata3/Kata3.java
+++ src/kata3/Kata3.java
@@ -1,25 +1,24 @@
 package kata3;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 public class Kata3 {
 
     public static void main(String[] args) {
         Histogram<String> histogram = new Histogram<>();
-        histogram.increment("gmail.com");
-        histogram.increment("gmail.com");
-        histogram.increment("gmail.com");
-        histogram.increment("ulpgc.es");
-        histogram.increment("ulpgc.es");
-        histogram.increment("ulpgc.es");
-        histogram.increment("ulpgc.es");
-        histogram.increment("ulpgc.es");
-        histogram.increment("hotmail.com");
-        histogram.increment("hotmail.com");
-        histogram.increment("hotmail.com");
-        histogram.increment("hotmail.com");
-        histogram.increment("hotmail.com");
-        histogram.increment("hotmail.com");
-        histogram.increment("hotmail.com");
+        String pathName = "C:\\Users\\usuario\\Documents\\NetBeansProjects\\Kata3\\Data\\emailsfilev1.txt";
         HistogramDisplay histo = new HistogramDisplay(histogram);
+        try {
+            BufferedReader fileIn = new BufferedReader(new FileReader(pathName));
+            String mail;
+            while ((mail=fileIn.readLine()) != null){
+                if (!mail.contains("@")) continue;
+                histogram.increment(mail.split("@")[1]);
+            }
+        } catch (Exception e) {
+            System.out.println("Fichero no existe"+ e);
+        }
         histo.execute();
     }
 }
