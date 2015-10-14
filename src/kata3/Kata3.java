991c65d3cd9f4dd3ef54dfc582db67aa5383792b
usuario <usuario@10.22.143.36>
2015-10-14 10:30:22 +0100

    Fourth commit

--- src/kata3/Kata3.java
+++ src/kata3/Kata3.java
@@ -2,23 +2,15 @@ package kata3;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.ArrayList;
 
 public class Kata3 {
 
     public static void main(String[] args) {
         Histogram<String> histogram = new Histogram<>();
         String pathName = "C:\\Users\\usuario\\Documents\\NetBeansProjects\\Kata3\\Data\\emailsfilev1.txt";
-        HistogramDisplay histo = new HistogramDisplay(histogram);
-        try {
-            BufferedReader fileIn = new BufferedReader(new FileReader(pathName));
-            String mail;
-            while ((mail=fileIn.readLine()) != null){
-                if (!mail.contains("@")) continue;
-                histogram.increment(mail.split("@")[1]);
-            }
-        } catch (Exception e) {
-            System.out.println("Fichero no existe"+ e);
-        }
-        histo.execute();
+        MailReader dominios = new MailReader(pathName);
+        ArrayList <String> domis = dominios.getDominiosArray();
+        Histogram<String> histograma = DominiosHistogramBuilder.execute(domis);
     }
 }
